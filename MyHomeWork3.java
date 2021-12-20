ac87a3d52c6131d863901ba481e4e03718ea0c63
Александра <95775931+AleksFe@users.noreply.github.com>
2021-12-21 00:01:30 +0300

    Update MyHomeWork3.java

--- MyHomeWork3.java
+++ MyHomeWork3.java
@@ -6,25 +6,51 @@
  */
 
 import java.util.Arrays;
-import java.util.Random;
 
 class MyHomeWork3 {
-    //static Random random = new Random();
 
     public static void main(String[]args) {
-        int[] arr = {0,1,1,0,0,1 } ;
-        for(int i=0; i < arr.length; i++)
-            if (arr[i]==0)
-            arr[i] = 1;
+
+        toStringArr1();
+        toStringArr2();
+        toStringArr3();
+        toStringArr4();
+    }
+
+        static void toStringArr1() {
+        int[] arr1 = {0,1,1,0,0,1 } ;
+        System.out.println(Arrays.toString(arr1));
+        for(int i=0; i < arr1.length; i++)
+            if (arr1[i]==0)
+            arr1[i] = 1;
             else
-            arr[i] = 0;
-            System.out.println(Arrays.toString(arr));
-    }
-    static void main(String[]args) {
-        int[] arr = new int[100];
-        for(int i = 1; i < arr.length; i++)
-            arr[i] = i + 1;
-            System.out.println(Arrays.toString(arr));
-    }
-    //static void main
+            arr1[i] = 0;
+            System.out.println(Arrays.toString(arr1));
+        }
+
+        static void toStringArr2() {
+        int[] arr2 = new int[100];
+        for(int i = 1; i < arr2.length; i++)
+            arr2[i] = i + 1;
+            System.out.println(Arrays.toString(arr2));
+        }
+
+        static void toStringArr3() {
+        int[] arr3 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        for (int i = 0; i < arr3.length; i++){
+            if (arr3[i] < 6) {
+                arr3[i]*= 2;
+            }
+        }
+            System.out.println(Arrays.toString(arr3));
+        }
+
+        static void toStringArr4() {
+        int[][] matrix = new int[4][4];
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][i] = 1;
+            matrix[i][matrix.length - i - 1] = 1;
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        }
 }
