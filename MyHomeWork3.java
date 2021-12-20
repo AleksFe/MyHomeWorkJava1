83537b641518c5a713e27e2c78412ede67e1487c
Александра <95775931+AleksFe@users.noreply.github.com>
2021-12-20 21:31:57 +0300

    Update MyHomeWork3.java

--- MyHomeWork3.java
+++ MyHomeWork3.java
@@ -9,14 +9,22 @@ import java.util.Arrays;
 import java.util.Random;
 
 class MyHomeWork3 {
-    static Random random = new Random();
+    //static Random random = new Random();
 
-    public static void main(String[] args) {
-        int a[] = { 0,1,1,0,0,1 } ;
-        for(int i=0; i < a.length; i++)
-        if (a[i]==0)
-        a[i] = 1;
-        else
-        a[i] = 0;}
-        System.out.println(Arrays.toString(arr))
+    public static void main(String[]args) {
+        int[] arr = {0,1,1,0,0,1 } ;
+        for(int i=0; i < arr.length; i++)
+            if (arr[i]==0)
+            arr[i] = 1;
+            else
+            arr[i] = 0;
+            System.out.println(Arrays.toString(arr));
+    }
+    static void main(String[]args) {
+        int[] arr = new int[100];
+        for(int i = 1; i < arr.length; i++)
+            arr[i] = i + 1;
+            System.out.println(Arrays.toString(arr));
+    }
+    //static void main
 }
